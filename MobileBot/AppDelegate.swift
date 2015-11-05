45737fa9dd7d06a04fa20062ce9b20751c282e76
Goran Vukovic <TheRealVuko@gmx.de>
2015-11-05 09:28:52 +0100

    Revert "Beacon eingearbeitet, Usecases müssen noch spezifiziert werden."
    This reverts commit 5ca7796e58ee2a1b1534159692049ef763c0e091.

--- MobileBot/AppDelegate.swift
+++ MobileBot/AppDelegate.swift
@@ -10,40 +10,37 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-    
+
     var window: UIWindow?
-    let beaconManager = BeaconManager(hash: "824905c09824e47314f2db7d8e7795f0");
-    let logger = StreamableLogger();
-    
+
+
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        
         // Override point for customization after application launch.
-        beaconManager.setDelegate(self);
-        beaconManager.start();
-        
-        logger.log(.Info, data: beaconManager.dbVersion());
-        
-        // loacal notification permission request
-        let types: UIUserNotificationType = [.Badge, .Alert, .Sound];
-        let notificationSettings = UIUserNotificationSettings(forTypes: types, categories: nil);
-        
-        UIApplication.sharedApplication().registerUserNotificationSettings(notificationSettings);
-        // loacal notification permission request
-        
-        UseCaseManager.sharedInstance().run();
-        
         return true
     }
-    
-    func application(application: UIApplication, didRegisterUserNotificationSettings notificationSettings: UIUserNotificationSettings) {
-        logger.log(.Info, data: notificationSettings);
+
+    func applicationWillResignActive(application: UIApplication) {
+        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+        // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
     }
-    
-    func application(application: UIApplication, performFetchWithCompletionHandler completionHandler: (UIBackgroundFetchResult) -> Void) {
-        beaconManager.startBackgroundFetch(completionHandler);
+
+    func applicationDidEnterBackground(application: UIApplication) {
+        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     }
-    
-    func application(application: UIApplication, didReceiveRemoteNotification userInfo: [NSObject : AnyObject]) {
-        logger.log(.Info, data: userInfo.description);
+
+    func applicationWillEnterForeground(application: UIApplication) {
+        // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
     }
+
+    func applicationDidBecomeActive(application: UIApplication) {
+        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    }
+
+    func applicationWillTerminate(application: UIApplication) {
+        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    }
+
+
 }
+
