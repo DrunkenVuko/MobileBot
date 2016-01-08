42f3765febba059386c9f0a38b3d09e6e3852ee7
Bianca <s59416@beuth-hochschule.de>
2016-01-08 11:28:08 +0100

    added draw-function, added mulitple walls

--- MobileBot/RaumvermesserViewController.swift
+++ MobileBot/RaumvermesserViewController.swift
@@ -13,6 +13,7 @@ class RaumvermesserViewController: UIViewController {
     var bn: BotNavigator?;
     let bcm = BotConnectionManager.sharedInstance();
     let logger = StreamableLogger();
+    
     var debounceTimer: NSTimer?
     var notification: UILocalNotification?;
     
@@ -27,6 +28,8 @@ class RaumvermesserViewController: UIViewController {
 
     // Zeit
     var counter = 0
+    // Zeit fuer eine einzelne Wand
+    var counterSingle = 0
     var counterMod = 0
     
     // Kontrollvariablen
@@ -36,6 +39,8 @@ class RaumvermesserViewController: UIViewController {
     var pingFront: Float = 0.0
     var whichWall: Int = 0
     var velocity: Float = 2
+    var finished: Bool = false
+    var log: Bool = true
 
     struct Wall {
         var ping: Float = 0
@@ -78,11 +83,13 @@ class RaumvermesserViewController: UIViewController {
     @IBOutlet weak var labelPingDistance: UILabel!
     @IBOutlet weak var labelServoAngle: UILabel!
 
+    @IBOutlet weak var containerFloorPlan: UIView!
     
     
     func updateCounter() {
         counter++
         counterMod = counter % 4
+        counterSingle++
         labelCounter.text = String(counter)
         labelFrontModulo.text = String(counterMod)
     }
@@ -111,11 +118,15 @@ class RaumvermesserViewController: UIViewController {
                 
                 if let bc = bc {
                     bn = BotNavigator(controller: bc);
-                    
+                    //bn?.setLogger(false)
                     bcm.connect(connection);
                 }
+                
+                //bc?.setLogger(false)
             }
         }
+        
+        //draw()
         
     }
     
@@ -133,13 +144,16 @@ class RaumvermesserViewController: UIViewController {
 
     // Raumvermesser App wird über Button "Vermessen" gestartet
     @IBAction func startMeasure(sender: AnyObject) {
-        logger.log(.Info, data: "Start Action Babysitter");
+        if(finished == false){
+            logger.log(.Info, data: "Start Action Raumvermesser");
+            self.whichWall = 0
         
+            self.timerCounter = NSTimer.scheduledTimerWithTimeInterval(1, target:self, selector: Selector("updateCounter"), userInfo: nil, repeats: true)
         
-        self.timerCounter = NSTimer.scheduledTimerWithTimeInterval(1, target:self, selector: Selector("updateCounter"), userInfo: nil, repeats: true)
-        
-
-        driveAndDetectWalls()
+            driveAndDetectWalls()
+        }else{
+            Toaster.show("Scan-Vorgang noch nicht beendet")
+        }
 
     }
     
@@ -162,7 +176,7 @@ class RaumvermesserViewController: UIViewController {
         if(stopMoving == true)
         {
             // Check first Wall
-            if(walls[self.whichWall].wallChecked == false)
+            if(self.whichWall < walls.count && walls[self.whichWall].wallChecked == false)
             {
                 self.stopOrDrive("yes")
                 //scanWallAndFront(walls[0].wallChecked)
@@ -184,6 +198,7 @@ class RaumvermesserViewController: UIViewController {
 
     func checkRight()
     {
+        printText("checkRight()");
         //bc?.scanRange(75, max: 90, inc: 3, callback: { [weak self] data in
         self.bc?.scanRange(160, max: 180, inc: 10, callback: { data in
             
@@ -195,7 +210,9 @@ class RaumvermesserViewController: UIViewController {
     func checkFront()
     
     {
-        var tempWall: Wall = Wall(number: whichWall)
+        printText("checkFront()");
+        //var tempWall: Wall = Wall(number: whichWall)
+        var tempWall: Wall = walls[self.whichWall]
         
         self.bc?.scanRange(65, max: 90, inc: 10, callback: { data in
             
@@ -215,8 +232,20 @@ class RaumvermesserViewController: UIViewController {
                 tempWall.ping = data.pingDistance
                 tempWall.wallChecked = true
                 tempWall.length = self.calcWallLength()
-                //self.updateWallValue(tempWall, i: self.whichWall)
+                
+                self.updateWallValue(tempWall, i: self.whichWall)
+                
                 self.turnLeft()
+                self.stopOrDrive("yes")
+
+                if(self.whichWall < 4){
+                    NSTimer.scheduledTimerWithTimeInterval(8, target:self, selector: Selector("driveAndDetectWalls"),
+                        userInfo: nil, repeats: false)
+                }else{
+                    self.finished = true
+                    //self.draw()
+                }
+                
             }
             else
             {
@@ -225,7 +254,7 @@ class RaumvermesserViewController: UIViewController {
         });
     }
     
-    func turnLeft()
+    func turnLeft( )
     {
         if let bn = bn {                   //bn.turnSpeed
             bn.turnToAngle(Float(90), speed: Float(15), completion: { [weak self] data in
@@ -238,13 +267,16 @@ class RaumvermesserViewController: UIViewController {
     
     func stopOrDrive(move: String)
     {
+        printText("stopOrDrive()");
         switch(move)
         {
             case "yes":
-                bc?.move(velocity, omega: 0, completion: nil);
+                printText("stopOrDrive() yes");
+                bc?.move(2, omega: 0, completion: nil);
             break
 
             case "no":
+                printText("stopOrDrive() no");
                 bc?.move(0, omega: 0, completion: nil);
             break
             
@@ -264,39 +296,97 @@ class RaumvermesserViewController: UIViewController {
     {
         self.timerScanRight.invalidate()
         self.timerScanFront.invalidate()
+        self.counterSingle = 0
     }
     
-    func updateWallValue(name: Wall, i: Int)
+    func updateWallValue(wall: Wall, i: Int)
     {
-        self.walls[i] =  name
-        self.whichWall++
-        print("Wall Updated!")
-        print("Wall: " + self.walls[i].name)
-        print("Distance: " + String(self.walls[i].ping))
-        print("Wall Number: " + String(self.walls[i].wall))
-        print("Wall Checked: " + String(self.walls[i].wallChecked))
-        print("Next Wall ->" + String(self.whichWall))
+        printText("updateWallValue int: "+String(i));
+        if(i < self.walls.count){
+            self.walls[i] =  wall
+            self.whichWall++
+            printText("Wall Updated!")
+            printText("Wall: " + self.walls[i].name)
+            printText("Distance: " + String(self.walls[i].ping))
+            printText("Wall Number: " + String(self.walls[i].wall))
+            printText("Wall Checked: " + String(self.walls[i].wallChecked))
+            printText("Next Wall ->" + String(self.whichWall))
+        }
     }
-    
     
     func calcWallLength()-> Float{
         //annhame: velocity: (float, cm/s)
-
-        var length: Float = self.velocity * Float(self.counter)
-        print("Wall Length: "+String(length))
+        //@todo pingdistance hinzufügen
+        var length: Float = self.velocity * Float(self.counterSingle)
+        printText("Wall Length: "+String(length))
         
         return length
     }
     
     
+    
+    func draw(){
+        let imageSize = CGSize(width: 100, height: 200)
+        let imageView = UIImageView(frame: CGRect(origin: CGPoint(x: 100, y: 100), size: imageSize))
+        //self.view.addSubview(imageView)
+        let image = drawCustomImage(imageSize)
+        imageView.image = image
+        
+        self.containerFloorPlan.addSubview(imageView)
+    }
+    
+    
+    func drawCustomImage(size: CGSize) -> UIImage {
+        // Setup our context
+        let bounds = CGRect(origin: CGPoint.zero, size: size)
+        let opaque = false
+        let scale: CGFloat = 0
+        UIGraphicsBeginImageContextWithOptions(size, opaque, scale)
+        let context = UIGraphicsGetCurrentContext()
+        
+        // Setup complete, do drawing here
+        CGContextSetStrokeColorWithColor(context, UIColor.redColor().CGColor)
+        CGContextSetLineWidth(context, 2.0)
+        
+        //CGContextStrokeRect(context, bounds)
+        
+        CGContextBeginPath(context)
+        CGContextMoveToPoint(context, 0, 0)
+        CGContextAddLineToPoint(context, 100,0)
+        //CGContextMoveToPoint(context, 100, 0)
+        CGContextAddLineToPoint(context, 100, 100)
+        //CGContextMoveToPoint(context, 100, 100)
+        CGContextAddLineToPoint(context, 50, 100)
+        //CGContextMoveToPoint(context, 50, 100)
+        CGContextAddLineToPoint(context, 50, 50)
+        //CGContextMoveToPoint(context, 50, 50)
+        CGContextAddLineToPoint(context, 0, 50)
+        //CGContextMoveToPoint(context, 0, 50)
+        CGContextAddLineToPoint(context, 0, 0)
+        CGContextMoveToPoint(context, 0, 25)
+        CGContextAddLineToPoint(context, 50, 25)
+
+        CGContextStrokePath(context)
+        
+        // Drawing complete, retrieve the finished image and cleanup
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return image
+    }
+    
+    
+    
+    func printText(message: String){
+        if(log){
+            print(message)
+        }
+    }
+    
+    
+    
+    
 
 }
-
-/*extension NSTimer {
-    var floatValue: Float {
-        return (self as NSTimer).floatValue
-    }
-}*/
 
 //timer = NSTimer.scheduledTimerWithTimeInterval(16.0, target: self, selector: "testScan", userInfo: nil, repeats: false)
 //
